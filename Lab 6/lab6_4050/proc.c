d4b9d542b511e375b1ba2b77441627ad1c2fa050
Fernando J. Bermúdez Medina <fernando.bermudez1@upr.edu>
2021-11-14 17:23:43 -0400

    Added Lab 7

--- Lab 6/lab6_4050/proc.c
+++ Lab 6/lab6_4050/proc.c
@@ -1,24 +1,6 @@
 //#include "proc.h"
 void enqueue(struct proc *rp	/* this process is now runnable */) {
-    /* Add 'rp' to one of the queues of runnable processes.  This function is
-     * responsible for inserting a process into one of the scheduling queues.
-     * The mechanism is implemented here.   The actual scheduling policy is
-     * defined in sched() and pick_proc().
-     *
-     * This function can be used x-cpu as it always uses the queues of the cpu the
-     * process is assigned to.
-     */
-    //int q = rp->p_priority;	 		/* scheduling queue to use */
-    //struct proc **rdy_head, **rdy_tail;
-
-    //assert(proc_is_runnable(rp));
-
-    //assert(q >= 0);
-
-    //rdy_head = get_cpu_var(rp->p_cpu, run_q_head);
-    //rdy_tail = get_cpu_var(rp->p_cpu, run_q_tail);
-
-/********** Your enqueue modification should be below ************/
+    /********** Your enqueue modification should be below ************/
     /* Now add the process to the queue. */
     /*If queue is empty, add just the single element to the queue, regardless of deadline*/
     if (!rdy_head[q]) {
@@ -55,35 +37,4 @@ void enqueue(struct proc *rp	/* this process is now runnable */) {
         }
     }
     /********* Your enqueue modification should be above ************/
-//   if (cpuid == rp->p_cpu) {
-//  	  /*
-//  	   * enqueueing a process with a higher priority than the current one,
-//  	   * it gets preempted. The current process must be preemptible. Testing
-//  	   * the priority also makes sure that a process does not preempt itself
-//  	   */
-//  	  struct proc * p;
-//  	  p = get_cpulocal_var(proc_ptr);
-//  	  assert(p);
-//  	  if((p->p_priority > rp->p_priority) &&
-//  			  (priv(p)->s_flags & PREEMPTIBLE))
-//  		  RTS_SET(p, RTS_PREEMPTED); /* calls dequeue() */
-//   }
-//  #ifdef CONFIG_SMP
-//   /*
-//     * if the process was enqueued on a different cpu and the cpu is idle, i.e.
-//     * the time is off, we need to wake up that cpu and let it schedule this new
-//     * process
-//     */
-//   else if (get_cpu_var(rp->p_cpu, cpu_is_idle)) {
-//  	  smp_schedule(rp->p_cpu);
-//   }
-//  #endif
-
-//   /* Make note of when this process was added to queue */
-//   read_tsc_64(&(get_cpulocal_var(proc_ptr)->p_accounting.enter_queue));
-
-
-//  #if DEBUG_SANITYCHECKS
-//   assert(runqueues_ok_local());
-//  #endif
 }
